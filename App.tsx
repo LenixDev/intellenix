2d41c07288171621bdebccdc463ef51fe93e31e2
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-04-08 22:35:26 +0100

    refactor(paths): replace '~' import aliases with '@' in source files and tsconfig

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import { TamaguiProvider } from '@tamagui/core'
-import { config } from '~/tamagui.config'
+import { config } from '@/tamagui.config'
 import { useColorScheme } from 'react-native'
-import { Signin } from '~app/signin'
-import { Home } from '~app/home'
+import { Signin } from '@/app/signin'
+import { Home } from '@/app/home'
 
 export default function App() {
 	const theme = useColorScheme()
