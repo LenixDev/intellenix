a84c4dcb8c0d426ff823553f8dbefaef72a122d8
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-04-12 19:27:27 +0100

    feat(chat): add Tasks component with selectable task list

---
+++ components/chat/tasks.tsx
@@ -0,0 +1,40 @@
+import { Check } from "@tamagui/lucide-icons-2";
+import { useMemo } from "react";
+import { Select } from "tamagui";
+import { Selection } from "../selection";
+
+const tasks = [
+	'programming',
+	'healthcare'
+] as const
+
+export const Tasks = () => (
+	<Selection
+		renderer={value => tasks.find(task => task === value) ?? 'ERR'}
+		defaultValue={tasks[1]}
+		listLabel='Tasks'
+		bg='transparent'
+		borderColor='transparent'
+		hoverStyle={{
+			background: '$background',
+			cursor: 'pointer',
+			borderColor: '$color6'
+		}}
+	>
+		{useMemo(
+			() => tasks.map((item, iter) => (
+				<Select.Item
+					index={iter}
+					key={item}
+					value={item}
+				>
+					<Select.ItemText>{item}</Select.ItemText>
+					<Select.ItemIndicator marginLeft='auto'>
+						<Check size={16} />
+					</Select.ItemIndicator>
+				</Select.Item>
+			)),
+			[tasks]
+		)}
+	</Selection>
+)
