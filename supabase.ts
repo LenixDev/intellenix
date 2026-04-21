e14f54ad805fe95dec83c5b055a5377bce338184
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-04-21 21:05:27 +0100

    feat(supabase): add @supabase/supabase-js dependency and supabase deployment scripts

---
+++ supabase.ts
@@ -0,0 +1,19 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createClient } from '@supabase/supabase-js'
+
+const { EXPO_PUBLIC_SUPABASE_URL, EXPO_PUBLIC_SUPABASE_KEY } = process.env
+if (typeof EXPO_PUBLIC_SUPABASE_URL !== 'string' || typeof EXPO_PUBLIC_SUPABASE_KEY !== 'string')
+	throw new Error('Could not find Supabase credentials')
+
+export const supabase = createClient(
+	EXPO_PUBLIC_SUPABASE_URL,
+	EXPO_PUBLIC_SUPABASE_KEY,
+	{
+		auth: {
+			storage: AsyncStorage,
+			autoRefreshToken: true,
+			persistSession: true,
+			detectSessionInUrl: false,
+		}
+	}
+)
