214bd076c8ec0f3b2df3d877875b0bc97f3825c9
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-04-22 17:45:33 +0100

    #

--- supabase.ts
+++ supabase.ts
@@ -1,22 +1,19 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
-const { EXPO_PUBLIC_SUPABASE_URL, EXPO_PUBLIC_SUPABASE_KEY } = process.env
-console.debug(process.env)
-if (typeof EXPO_PUBLIC_SUPABASE_URL !== 'string' || typeof EXPO_PUBLIC_SUPABASE_KEY !== 'string')
+const supabaseUrl = process.env['EXPO_PUBLIC_SUPABASE_URL']
+const supabaseKey = process.env['EXPO_PUBLIC_SUPABASE_KEY']
+
+if (typeof supabaseUrl !== 'string' || typeof supabaseKey !== 'string')
 	throw new Error('Supabase credential/s is/are undefined', {
-		cause: { EXPO_PUBLIC_SUPABASE_URL, EXPO_PUBLIC_SUPABASE_KEY }
+		cause: { supabaseUrl, supabaseKey }
 	})
 
-export const supabase = createClient(
-	EXPO_PUBLIC_SUPABASE_URL,
-	EXPO_PUBLIC_SUPABASE_KEY,
-	{
-		auth: {
-			storage: AsyncStorage,
-			autoRefreshToken: true,
-			persistSession: true,
-			detectSessionInUrl: false,
-		}
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+	auth: {
+		storage: AsyncStorage,
+		autoRefreshToken: true,
+		persistSession: true,
+		detectSessionInUrl: false,
 	}
-)
+})
