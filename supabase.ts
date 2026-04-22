38bb7c94cbf137d3e028096e9483452ab069e9c7
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-04-22 17:36:27 +0100

    #

--- supabase.ts
+++ supabase.ts
@@ -2,6 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
 const { EXPO_PUBLIC_SUPABASE_URL, EXPO_PUBLIC_SUPABASE_KEY } = process.env
+console.debug(process.env)
 if (typeof EXPO_PUBLIC_SUPABASE_URL !== 'string' || typeof EXPO_PUBLIC_SUPABASE_KEY !== 'string')
 	throw new Error('Supabase credential/s is/are undefined', {
 		cause: { EXPO_PUBLIC_SUPABASE_URL, EXPO_PUBLIC_SUPABASE_KEY }
