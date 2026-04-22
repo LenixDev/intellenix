ba9a5a7ff73566de916e12679675ace1e5f99ae6
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-04-22 17:27:39 +0100

    #

--- supabase.ts
+++ supabase.ts
@@ -3,7 +3,9 @@ import { createClient } from '@supabase/supabase-js'
 
 const { EXPO_PUBLIC_SUPABASE_URL, EXPO_PUBLIC_SUPABASE_KEY } = process.env
 if (typeof EXPO_PUBLIC_SUPABASE_URL !== 'string' || typeof EXPO_PUBLIC_SUPABASE_KEY !== 'string')
-	throw new Error('Could not find Supabase credentials')
+	throw new Error('Could not find Supabase credentials', {
+		cause: { EXPO_PUBLIC_SUPABASE_URL, EXPO_PUBLIC_SUPABASE_KEY }
+	})
 
 export const supabase = createClient(
 	EXPO_PUBLIC_SUPABASE_URL,
