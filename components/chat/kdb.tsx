59979ee1bc8b617ad04d616c17c27f40a9380c48
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-05-04 19:57:27 +0100

    refactor(kdb): collapse multiline Text components into single-line JSX

--- components/chat/kdb.tsx
+++ components/chat/kdb.tsx
@@ -8,19 +8,13 @@ export const Kdb = ({ isMac }: { isMac: boolean }) => {
 	return (
 		<XStack flexDirection='row' items='center' gap='$3'>
 			<View flexDirection='row' items='center' gap='$1'>
-				<Text color='$color06' fontSize='$1'>
-					{t('press')}
-				</Text>
+				<Text color='$color06' fontSize='$1'>{t('press')}</Text>
 				<Kbd size={10}>Tab</Kbd>
-				<Text color='$color06' fontSize='$1'>
-					{t('to_focus')}
-				</Text>
+				<Text color='$color06' fontSize='$1'>{t('to_focus')}</Text>
 			</View>
 			<Separator vertical height={12} borderColor='$color02' />
 			<View flexDirection='row' gap='$1' items='center'>
-				<Text color='$color06' fontSize='$1'>
-					{t('press')}
-				</Text>
+				<Text color='$color06' fontSize='$1'>{t('press')}</Text>
 				<View flexDirection='row' items='center'>
 					<Kbd size={10}>
 						{isMac ?
@@ -30,9 +24,7 @@ export const Kdb = ({ isMac }: { isMac: boolean }) => {
 					<Text> + </Text>
 					<Kbd size={10}>Enter</Kbd>
 				</View>
-				<Text color='$color06' fontSize='$1'>
-					{t('to_send')}
-				</Text>
+				<Text color='$color06' fontSize='$1'>{t('to_send')}</Text>
 			</View>
 		</XStack>
 	)
